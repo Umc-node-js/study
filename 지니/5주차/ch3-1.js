0038cedd0e720c1967b4de7b97792ed8968619da
eojin0814 <leeeojin6@gmail.com>
2023-05-06 22:57:16 +0900

    5주차 과제

---
+++ 지니/5주차/ch3-1.js
@@ -0,0 +1,16 @@
+// 자바 스크립트의 객체와 함수이해하기
+//자료형을 명시하지 않는 언어
+
+
+//Boolean 
+//Number 정수나 부동소수 값을 가지는 자료형
+
+//Undefind 값을 할당하지 않으면 
+
+//변수 만들기
+var name;
+var age = 20;
+
+console.log(name);
+console.log(age);
+name = 'jinny';
